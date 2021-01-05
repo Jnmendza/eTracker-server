19b7e98c2d29a61558dc1b1634dd0ef87d97c63e
Jnmendza <jonatmendoza@gmail.com>
2021-01-04 19:51:13 -0800

    CORS function added

--- server.js
+++ server.js
@@ -22,4 +22,10 @@ app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-ALlow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+});
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
