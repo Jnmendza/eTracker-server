56b56320efb7a40c83061a3c4de4c36c4425eed6
Jnmendza <jonatmendoza@gmail.com>
2021-01-04 19:44:21 -0800

    routes back to default

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactionController')
 
 router
-    .route('https://etracker-app.herokuapp.com')
+    .route('/')
     .get(getTransactions)
     .post(addTransaction);
 
 router
-    .route('https://etracker-app.herokuapp.com/:id')
+    .route('/:id')
     .delete(deleteTransaction)
 
 module.exports = router;
